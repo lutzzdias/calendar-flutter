04ab9dfc92bdca16cff046299648f7d26c098c84
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-07-29 09:05:35 -0400

    Created MealController

---
+++ lib/Presentation/Controllers/Meal/meal_controller.dart
@@ -0,0 +1,24 @@
+import 'package:calendar/Application/Services/meal_service.dart';
+import 'package:calendar/Domain/DTOs/Meal/create_meal_dto.dart';
+import 'package:calendar/Domain/DTOs/Meal/meal_dto.dart';
+import 'package:calendar/Domain/DTOs/Meal/meal_response_dto.dart';
+
+class MealController {
+  MealService _mealService = MealService();
+
+  MealResponseDTO createMeal(CreateMealDTO createMealDTO) {
+    return _mealService.createMeal(createMealDTO);
+  }
+
+  MealDTO? getMealById(String id) {
+    return _mealService.getMealById(id);
+  }
+
+  MealResponseDTO updateMeal(MealDTO mealDTO) {
+    return _mealService.updateMeal(mealDTO);
+  }
+
+  MealResponseDTO deleteMeal(String id) {
+    return _mealService.deleteMeal(id);
+  }
+}
