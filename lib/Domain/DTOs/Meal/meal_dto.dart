1f108334e05caa7474553d93eb51a62db2ec67bc
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-07-29 08:52:50 -0400

    Create fromMeal constructor in DTO

--- lib/Domain/DTOs/Meal/meal_dto.dart
+++ lib/Domain/DTOs/Meal/meal_dto.dart
@@ -1,15 +1,22 @@
 import 'package:calendar/Domain/Enums/meal_type_enum.dart';
+import 'package:calendar/Domain/Models/meal.dart';
 
 class MealDTO {
-  String id;
+  String? id;
   DateTime date;
   MealType mealType;
   List<String> foodsIds;
 
   MealDTO({
-    required this.id,
+    this.id,
     required this.date,
     required this.mealType,
     List<String>? foodsIds,
   }) : foodsIds = foodsIds ?? List.empty(growable: true);
+
+  MealDTO.fromMeal(Meal meal)
+      : id = meal.id,
+        date = meal.date,
+        mealType = meal.mealType,
+        foodsIds = meal.foodsIds;
 }
