5a8183a6368744c9b495d88b26bfdad665bd87ca
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-07-29 08:19:41 -0400

    Changed list of foods to list o foodIds

--- lib/Domain/Models/meal.dart
+++ lib/Domain/Models/meal.dart
@@ -5,14 +5,7 @@ class Meal {
   String id;
   DateTime date;
   MealType mealType;
-  List<Food> foods = List.empty(growable: true);
+  List<String> foodsIds = List.empty(growable: true);
 
-  Meal({
-    required this.id,
-    required this.date,
-    required this.mealType,
-    Food? food,
-  }) {
-    if (food != null) foods.add(food);
-  }
+  Meal({required this.id, required this.date, required this.mealType});
 }
