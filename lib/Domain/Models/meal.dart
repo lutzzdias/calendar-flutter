2c7290d0fcc9f6be34f7d6356ee416afb4f5fdf3
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-07-29 08:48:25 -0400

    Made meal id nullable and not required

--- lib/Domain/Models/meal.dart
+++ lib/Domain/Models/meal.dart
@@ -1,13 +1,13 @@
 import 'package:calendar/Domain/Enums/meal_type_enum.dart';
 
 class Meal {
-  String id;
+  String? id;
   DateTime date;
   MealType mealType;
   List<String> foodsIds;
 
   Meal({
-    required this.id,
+    this.id,
     required this.date,
     required this.mealType,
     List<String>? foodsIds,
