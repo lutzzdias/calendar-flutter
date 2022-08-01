fc61f4de3e2e96d6477e401caa470500178a3976
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-08-01 08:43:46 -0400

    made dayPage stateful

--- lib/Presentation/Pages/calendar_page.dart
+++ lib/Presentation/Pages/calendar_page.dart
@@ -11,7 +11,7 @@ class CalendarPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             Expanded(
               child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
--- lib/Presentation/Pages/day_page.dart
+++ lib/Presentation/Pages/day_page.dart
@@ -1,16 +1,38 @@
+import 'package:calendar/Domain/Models/meal.dart';
+import 'package:calendar/Presentation/Widgets/DayPage/meal_button.dart';
 import 'package:flutter/material.dart';
 
-class DayPage extends StatelessWidget {
+class DayPage extends StatefulWidget {
   final int day;
+
   const DayPage({Key? key, required this.day}) : super(key: key);
+
+  @override
+  State<DayPage> createState() => _DayPageState();
+}
+
+class _DayPageState extends State<DayPage> {
+  List<Meal> meals = List.empty(growable: true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("$day"),
+        title: Text("${widget.day}"),
       ),
-      body: Center(child: Text("$day")),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          MealButton(
+              title: "Café da manhã", icon: Icon(Icons.breakfast_dining)),
+          SizedBox(height: 10),
+          MealButton(title: "Almoço", icon: Icon(Icons.lunch_dining)),
+          SizedBox(height: 10),
+          MealButton(title: "Jantar", icon: Icon(Icons.dinner_dining)),
+          SizedBox(height: 10),
+          MealButton(title: "Lanche", icon: Icon(Icons.fastfood)),
+        ],
+      ),
     );
   }
 }
