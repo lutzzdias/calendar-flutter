8723630fa796351f98070be7b655307bfe1e9f32
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-08-03 13:07:30 -0400

    Implemented create meal and print test getMeal

--- lib/Presentation/Widgets/DayPage/meal_button.dart
+++ lib/Presentation/Widgets/DayPage/meal_button.dart
@@ -1,12 +1,16 @@
+import 'package:calendar/Domain/DTOs/Meal/create_meal_dto.dart';
+import 'package:calendar/Presentation/Controllers/Meal/meal_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MealButton extends StatelessWidget {
+  late final MealController _mealController;
   final String title;
   final Icon icon;
   final DateTime date;
   final String mealType;
 
-  const MealButton(
+  MealButton(
       {Key? key,
       required this.title,
       required this.icon,
@@ -16,9 +20,14 @@ class MealButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _mealController = Provider.of<MealController>(context);
     return TextButton.icon(
       style: TextButton.styleFrom(minimumSize: const Size.fromHeight(75)),
-      onPressed: () => null,
+      onPressed: () {
+        final mealInDb = _mealController
+            .createMeal(CreateMealDTO(date: date, mealType: mealType));
+        print(_mealController.getMealById(mealInDb.meal!.id)!.id);
+      },
       icon: icon,
       label: Text(title),
     );
