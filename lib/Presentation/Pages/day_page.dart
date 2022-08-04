349683c3a48a1b297d4669b5cd4b2c8d26406431
Thiago Lutz Dias <estagio.sst19@sesims.com.br>
2022-08-04 08:54:49 -0400

    Improved createMeal and implemented deleteMeal in frontend

--- lib/Presentation/Pages/day_page.dart
+++ lib/Presentation/Pages/day_page.dart
@@ -1,6 +1,8 @@
-import 'package:calendar/Domain/Models/meal.dart';
+import 'package:calendar/Domain/DTOs/Meal/meal_dto.dart';
+import 'package:calendar/Presentation/Controllers/Meal/meal_controller.dart';
 import 'package:calendar/Presentation/Widgets/DayPage/meal_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DayPage extends StatefulWidget {
   final DateTime date;
@@ -12,10 +14,17 @@ class DayPage extends StatefulWidget {
 }
 
 class _DayPageState extends State<DayPage> {
-  List<Meal> meals = List.empty(growable: true);
-
   @override
   Widget build(BuildContext context) {
+    final mealController = Provider.of<MealController>(context);
+    List<MealDTO> breakfast = mealController
+        .getAllMeals()
+        .where((element) => element.mealType == "breakfast")
+        .where(
+          (element) => element.date == widget.date,
+        )
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("${widget.date.day}"),
@@ -28,13 +37,15 @@ class _DayPageState extends State<DayPage> {
             icon: const Icon(Icons.breakfast_dining),
             date: widget.date,
             mealType: "breakfast",
+            mealController: mealController,
           ),
-          const SizedBox(height: 10),
+          showMeal(context, breakfast, mealController),
           MealButton(
             title: "Almoço",
             icon: const Icon(Icons.lunch_dining),
             date: widget.date,
             mealType: "lunch",
+            mealController: mealController,
           ),
           const SizedBox(height: 10),
           MealButton(
@@ -42,6 +53,7 @@ class _DayPageState extends State<DayPage> {
             icon: const Icon(Icons.dinner_dining),
             date: widget.date,
             mealType: "dinner",
+            mealController: mealController,
           ),
           const SizedBox(height: 10),
           MealButton(
@@ -49,9 +61,38 @@ class _DayPageState extends State<DayPage> {
             icon: const Icon(Icons.fastfood),
             date: widget.date,
             mealType: "snack",
+            mealController: mealController,
           ),
         ],
       ),
     );
+  }
+
+  Widget showMeal(BuildContext context, List<MealDTO> breakfast,
+      MealController mealController) {
+    return breakfast.isNotEmpty
+        ? Container(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(breakfast.first.id.toString()),
+                    Text(breakfast.first.mealType),
+                    Text(breakfast.first.date.toString()),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => setState(
+                      () => mealController.deleteMeal(breakfast.first.id)),
+                )
+              ],
+            ),
+          )
+        : const SizedBox(height: 10);
   }
 }
